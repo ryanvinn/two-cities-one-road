afd813d1bb919df3315bb7dc3aad7f12e9356071
arthur-vasco7 <arthur.vasconcelos.rego.barros@ccc.ufcg.edu.br>
2025-07-30 22:51:55 -0300

    envio parcial 2

--- src/game.hs
+++ src/game.hs
@@ -1,33 +1,50 @@
+-- definicao de tipos compostos e `alias` para facilitar legibilidade
 data Terrain = Plains | River | Mountain deriving (Eq, Show)
 type Cords = (Int, Int)
-
-data Tile = Tile { terrain :: Terrain, buildingCost :: Int, passingCost :: Int} deriving (Eq, Show)
+data Tile = Tile { terrain :: Terrain, buildingCost :: Int, passingCost :: Int, location :: Cords, built :: Bool} deriving (Eq, Show)
 -- funcoes seletoras: <atributo> <tipo>
-
 type Map = [[Tile]]
 
+-- TODO: melhorar geracao de mapa (noise?)
 createMap :: Cords -> Map
-createMap (maxLat, maxLong) = [[tile lat long | lat <- [0..maxLat]] | long <- [0..maxLong]]
-    where 
-        tile lat long 
-            | odd lat && even long = Tile Mountain 3 2
-            | mod long 3 == 0 = Tile River 2 2
-            | otherwise = Tile Plains 1 1
+createMap (maxLat, maxLong) = [[tile lat long | lat <- [0..maxLat-1]] | long <- [0..maxLong-1]]
+  where 
+    tile lat long
+      | odd lat && even long = Tile Mountain 3 2 (lat, long) False
+      | mod long 3 == 0 = Tile River 2 2 (lat, long) False
+      | otherwise = Tile Plains 1 1 (lat, long) False
 
+-- TODO: escolher icones melhores (emojis?)
 printTile :: Tile -> Char
-printTile tile = case terrain tile of
-  Plains   -> '.'
-  River    -> '~'
-  Mountain -> '^'
+printTile tile
+  | built tile = 'O'
+  | otherwise = case terrain tile of
+    Plains -> '.'
+    River -> '~'
+    Mountain -> '^'
 
+--printMap: mapeia a funcao printTile em todas as listas internas da matriz, e chama recursivamente para o restante
 printMap :: Map -> IO ()
-printMap = mapM_ (putStrLn . map printTile)
+printMap [] = print "##########"
+printMap (col: cols) = do
+  putStrLn (map printTile col)
+  printMap cols
 
-neighbors :: Tile -> [Tile, Tile, Tile, Tile]
-neighbors curent =
+getElement :: Cords -> Map -> Tile
+getElement (lat, long) mp = mp !! lat !! long
 
+findNeighbors :: Cords -> Map -> [Cords]
+findNeighbors (lat, long) mp = [(lat -1, long), (lat +1, long), (lat, long -1), (lat, long +1)]
+
+-- funcao build retorna o mesmo tile, so que com atributo de construido
+buildTile :: Cords -> Map -> Tile
+buildTile cords mp = newTile 
+  where
+    newTile = Tile (terrain current) (buildingCost current) (passingCost current) (location current) True 
+      where current = getElement cords mp
 
 main :: IO()
 main = do
-    let mapa = createMap (50, 5)
+    let mapa = createMap (5, 5)
+    print "##########"
     printMap mapa
