e6b63b8efb0016a438b2d1cd338e737574818c34
arthur-vasco7 <arthur.vasconcelos.rego.barros@ccc.ufcg.edu.br>
2025-08-06 23:08:46 -0300

    update

--- src/game.hs
+++ src/game.hs
@@ -7,12 +7,17 @@ type Map = [[Tile]]
 
 -- TODO: melhorar geracao de mapa (noise?)
 createMap :: Cords -> Map
-createMap (maxLat, maxLong) = [[tile lat long | lat <- [0..maxLat-1]] | long <- [0..maxLong-1]]
+createMap (maxLat, maxLong) = [[tile lat long | long <- [0..maxLong-1]] | lat <- [0..maxLat-1]]
   where 
     tile lat long
       | odd lat && even long = Tile Mountain 3 2 (lat, long) False
       | mod long 3 == 0 = Tile River 2 2 (lat, long) False
       | otherwise = Tile Plains 1 1 (lat, long) False
+
+verifyCord :: Cords -> Map -> Bool
+verifyCord (lat, long) mp =
+  lat >= 0 && lat < length mp &&
+  long >= 0 && long < length (head mp)
 
 -- TODO: escolher icones melhores (emojis?)
 printTile :: Tile -> Char
@@ -43,8 +48,29 @@ buildTile cords mp = newTile
     newTile = Tile (terrain current) (buildingCost current) (passingCost current) (location current) True 
       where current = getElement cords mp
 
+updateGrid :: Cords -> Map -> Map
+updateGrid (lat, long) mp =
+  let row = mp !! lat
+      newRow = take long row ++ [buildTile (lat, long) mp] ++ drop (long + 1) row
+  in take lat mp ++ [newRow] ++ drop (lat + 1) mp
+
+readPlayer :: String -> Maybe Cords
+readPlayer input = 
+  case words input of [a, b] -> 
+    case (reads a, reads b) of ((lat, ""):_, (long, ""):_) -> 
+    Just (lat, long)
+    _ -> Nothing
+  _ -> Nothing
+
 main :: IO()
 main = do
     let mapa = createMap (5, 5)
     print "##########"
     printMap mapa
+
+    let mapa2 = updateGrid (2, 3) mapa
+
+    printMap mapa2
+
+    --print (getElement (2, 3) mapa)
+    --print (getElement (3, 2) mapa)
