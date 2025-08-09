ec9652478cabc759715e26177de751a8e58155f3
ryanvinn <ryan.tielles@protonmail.com>
2025-08-09 10:17:52 -0300

    Algumas mudanças no módulo de mapas e criação da função de geração de mapa aleatório

--- src/Map.hs
+++ src/Map.hs
@@ -1,5 +1,9 @@
 module Map where
 
+import System.Random (randomRIO)
+
+import Persistence
+
 -- | Gera e retorna numeração de um novo mapa
 generateRandomMap :: Int
 generateRandomMap = 0 -- TODO fila circular
@@ -12,6 +16,17 @@ getSavedMapsInfos = [""] -- TODO
 getGameHeader :: Int -> String
 getGameHeader map_number = "" -- TODO
 
--- | Retorna informações de matrix do mapa em questão
+-- | Retorna informações de matriz do mapa em questão
 getGameMatrix :: Int -> [String]
 getGameMatrix map_number = [""] -- TODO
+
+-- | Gera e retorna um mapa aleatório
+getRandomMap :: IO String
+getRandomMap = do
+  middle <- sequence (replicate (11*5 - 2) (randomChar alphabet))
+  return ("C" ++ middle ++ "C")
+  where
+    randomChar chars = do
+      i <- randomRIO (0, length chars - 1)
+      return (chars !! i)
+    alphabet = "PPPPMML"
